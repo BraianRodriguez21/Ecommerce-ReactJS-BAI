908377e26a69d49e7d45907cd4765f7ccd9a8dae
BraianRodriguez21 <braianrodriguezomg@gmail.com>
2024-02-19 17:55:27 -0300

    corrigiendo errores

--- proyect-react/src/Components/carts/Cart.jsx
+++ proyect-react/src/Components/carts/Cart.jsx
@@ -1,4 +1,23 @@
+// import React from "react";
+// import productos from "../Product/Productos.json";
+
+// export default function Cart() {
+//   return (
+//     <section>
+//       <div>
+//         {productos.map((product) => (
+//           <div key={product.id}>
+//             <h2>{product.name}</h2>
+//             <p>{product.description}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </section>
+//   );
+// }
+
 import React from "react";
+import { Link } from "react-router-dom";
 import productos from "../Product/Productos.json";
 
 export default function Cart() {
@@ -9,6 +28,7 @@ export default function Cart() {
           <div key={product.id}>
             <h2>{product.name}</h2>
             <p>{product.description}</p>
+            <Link to={`/product/${product.id}`}>View Details</Link>
           </div>
         ))}
       </div>
