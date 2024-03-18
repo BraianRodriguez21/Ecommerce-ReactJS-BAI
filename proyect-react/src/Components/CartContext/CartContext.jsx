065a30fbecd02f3993b6808ce39bdda50f876fd9
BraianRodriguez21 <braianrodriguezomg@gmail.com>
2024-03-17 23:11:43 -0300

    2

--- proyect-react/src/Components/CartContext/CartContext.jsx
+++ proyect-react/src/Components/CartContext/CartContext.jsx
@@ -1,49 +1,34 @@
-import { createContext, useState,useEffect } from "react";
-
-
-export const CartContext = createContext(null);
-
+export const CartContext = createContext();
+const { Provider } = CartContext;
 export const ShoppingCartProvider = ({ children }) => {
+    const [cart,setCart] = useState([])
+    const isItemFound =  cart.find((items) => items.id === items.id); 
+   const  addToCart = ()=>{
 
-    
-    const [cart, setCart] = useState([]);
-    
-    useEffect(() => {
-    },[setCart]);
-
-
-    const total = () => {
-        let total = 0;
-        cart.forEach(prod => {
-          total = total + prod.price * prod.quantity
-        })
-        return total;
+      if (isItemFound) {
+        return cart.map((i) => {
+          if (i.id === item.id) {
+            setCart([...cart,{...item,quantity: item.quantity +1}])
+          } 
+        });
+      } else {
+        setCart([...cart, { items }])
       }
 
-    const removeAll = () => {
-        setCart([]);
     }
+ 
 
-    // const addToCart = (item) => {
-    //   console.log("hola")
-    //       const isItemFound =  cart.find((items) => items.id === item.id); 
-    //       if (isItemFound) {
-    //         return cart.map((i) => {
-    //           if (i.id === item.id) {
-    //             setCart([...cart,{...item,quantity: item.quantity +1}])
-    //           } 
-    //         });
-    //       } else {
-    //         setCart([...cart, { item }])
-    //       }
-       
-    //   }; 
-
+    const valorDelContexto = {
+      addToCart
+    }
     return (
-        <CartContext.Provider value={{cart,setCart,removeAll,total}}>
+        <Provider value={valorDelContexto}>
             {children}
-        </CartContext.Provider>
-    );
-};
+        </Provider>
+    )
 
-export default CartContext;
+}
+
+
+
+export default ShoppingCartProvider;
